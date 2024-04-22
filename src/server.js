601187849166933a3af483bc50c5e02459577128
marcelorpc <marcelorpchaves@gmail.com>
2024-04-22 11:09:02 -0300

    post json method test

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.post("/users", (request, response) => {
-  const {name, email, password} = request.body
+  const {name, email, password} = request.body;
 
-  response.send(`
-    User: ${name}
-    Email: ${email}
-    password: ${password}
-  `)
+  response.json({name, email, password});
 })
 
 const PORT = 3333;
